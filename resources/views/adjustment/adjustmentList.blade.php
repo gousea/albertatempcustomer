77de2e085a3fd924eb3af2608eeb72e905608387
sumitbadal <manishranbir774@gmail.com>
2021-05-12 19:38:02 +0530

    adjustment list new design

--- resources/views/adjustment/adjustmentList.blade.php
+++ resources/views/adjustment/adjustmentList.blade.php
@@ -137,8 +137,8 @@
 @section('page-script')
 
 
-<link href = "https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel = "stylesheet">
-<script src = "https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+<link href = "https://code.jquery.com/ui/1.12.1/themes/ui-lightness/jquery-ui.css" rel = "stylesheet">
+<script src = "https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
 <script>
     $(function() {
