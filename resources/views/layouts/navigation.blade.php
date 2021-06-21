8033a2f3619ae8c4127d8f274cd5a81bd14f981a
hemanthalberta <hemanth.kumar@aroha.co.in>
2021-06-21 20:49:12 +0530

    cleared bugs employee

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -57,7 +57,7 @@
                                         Promotions </a></li>
                                 <li><a class="dropdown-item sub-dropdown text-uppercase" href="{{ url('buydown') }}"> Buy Down </a></li>
                                 <li><a class="dropdown-item sub-dropdown text-uppercase" href="#"> Item Group </a></li>
-                                <li><a class="dropdown-item sub-dropdown text-uppercase" href="#"> Quick Item </a></li>
+                                <li><a class="dropdown-item sub-dropdown text-uppercase" href="{{ url('/item/quick_item_list') }}"> Quick Item </a></li>
                             </ul>
                         </li>
                     @endif
