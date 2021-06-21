0fa454e009694e37d4a2a75aa6c06724de5768c2
root <root@ip-10-0-0-227.us-east-2.compute.internal>
2021-06-21 08:39:03 +0000

    done

--- resources/views/sale.blade.php
+++ resources/views/sale.blade.php
@@ -19,59 +19,59 @@
                     <table width="100%" border="0" cellspacing="5" cellpadding="0" style="border-bottom:1px solid #999;">
                       <tr style="line-height:25px;">
                           <td width="50%" align="left">
-                          <strong>Date</strong> 06-14-2021
+                          <strong>Date</strong> 06-17-2021
                           </td>
                           <td width="25%" align="right">
                               <strong>Order Number</strong>
                           </td>
-                          <td>&nbsp;&nbsp;2106101021</td>
+                          <td>&nbsp;&nbsp;21053010185</td>
                       </tr>
                       <tr style="line-height:25px;">
-                        <td width="50%" align="left"><strong>Store Name: </strong>ALBERTA DEMO</td>
+                        <td width="50%" align="left"><strong>Store Name: </strong>LU-CO TOBACCO Outlet</td>
                         <td width="25%" align="right"><strong>Status</strong></td>
                         <td>&nbsp;&nbsp;Open</td>
                       </tr>
                       <tr style="line-height:25px;">
-                        <td width="50%" align="left"><strong>Store Address: </strong>68 Culver Rd</td>
+                        <td width="50%" align="left"><strong>Store Address: </strong>204 North Hampton St</td>
                         <td width="25%" align="right"><strong>Ordered</strong></td>
-                        <td>&nbsp;&nbsp;06-10-2021</td>
+                        <td>&nbsp;&nbsp;05-30-2021</td>
                       </tr>
                       <tr style="line-height:25px;">
                         <td width="50%" align="left"><strong></strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        Monmouth Junction NJ 89789
+                        Easton PA 18042
                         </td>
                         <td width="25%" align="right"><strong>Invoiced</strong></td>
-                        <td>&nbsp;&nbsp;06-10-2021</td>
+                        <td>&nbsp;&nbsp;05-30-2021</td>
                       </tr>
                       <tr style="line-height:25px;">
                         <td width="50%" align="left"><strong>Store Phone: </strong></td>
                         <td width="25%" align="right"><strong>Sale Number</strong></td>
-                        <td>&nbsp;&nbsp;2106101021</td>
+                        <td>&nbsp;&nbsp;21053010185</td>
                       </tr>
                       <tr style="line-height:25px;">
                         <td width="50%" align="left">&nbsp;</td>
                         <td width="25%" align="right"><strong>Sales Person</strong></td>
-                        <td>&nbsp;&nbsp;Admin  (101)</td>
+                        <td>&nbsp;&nbsp;Divyesh (101)</td>
                       </tr>
                       <tr style="line-height:25px;">
                         <td width="50%" align="left">&nbsp;</td>
                         <td width="25%" align="right"><strong>Tender Type</strong></td>
-                        <td>&nbsp;&nbsp;Cash</td>
+                        <td>&nbsp;&nbsp;Credit Card</td>
                       </tr>
                       <tr style="line-height:25px;">
                         <td width="50%" align="left">&nbsp;</td>
                         <td width="25%" align="right"><strong>TRN</strong></td>
-                        <td>&nbsp;&nbsp;2106101021</td>
+                        <td>&nbsp;&nbsp;21053010185</td>
                       </tr>
                       <tr style="line-height:25px;">
                         <td width="50%" align="left">&nbsp;</td>
                         <td width="25%" align="right"><strong>TRN Time</strong></td>
-                        <td>&nbsp;&nbsp;06-10-2021 11:29 AM</td>
+                        <td>&nbsp;&nbsp;05-30-2021 05:57 PM</td>
                       </tr>
                       <tr style="line-height:25px;">
                         <td width="50%" align="left">&nbsp;</td>
                         <td width="25%" align="right"><strong>Register</strong></td>
-                        <td>&nbsp;&nbsp;102</td>
+                        <td>&nbsp;&nbsp;101</td>
                       </tr>
                       <tr style="line-height:25px;">
                         <td width="50%" align="left"><strong>Bill</strong>&nbsp;</td>
@@ -110,13 +110,13 @@
                 <td><table width="100%" border="0" cellspacing="0" cellpadding="0"  style="margin:15px 0px;"><tr><th>Qty</th><th>Pack</th><th>SKU</th><th>Item Name</th><th>Size</th><th>Unit Price</th><th>Total Price</th></tr>
          
                         <tr>
-                            <td>1.00</td>
+                            <td>3.00</td>
                             <td>1</td>
-                            <td>3</td>
-                            <td>Non Taxable Item</td>
+                            <td>078250003287</td>
+                            <td>*Steel Resereve Regular 24 oz Can - 24 oz</td>
                             <td></td>
-                            <td>0.01</td>
-                            <td>0.01</td>
+                            <td>1.99</td>
+                            <td>5.97</td>
                         </tr></table></td>
               </tr><hr style="border-top:1px solid #999;">
               <tr style="border-bottom:1px solid #999;">
@@ -124,7 +124,7 @@
                   <tr>
                     <td width="70%" align="right" style="padding-bottom:5px;"><strong>Sub Total</strong></td>
                     <td width="10%" style="padding-bottom:5px;">&nbsp;</td>
-                    <td width="20%" align="right" style="padding-bottom:5px;">0.01</td>
+                    <td width="20%" align="right" style="padding-bottom:5px;">5.97</td>
                   </tr>
                   <tr style="border-top:1px solid #999;">
                     <td width="70%" align="right" style="padding-top:5px;padding-bottom:5px;"><strong>Tax</strong></td>
@@ -134,15 +134,15 @@
                   <tr style="border-top:1px solid #999;border-bottom:1px solid #999;">
                     <td width="70%" align="right" style="padding-top:5px;padding-bottom:5px;"><strong>Total</strong></td>
                     <td width="10%" style="padding-top:5px;padding-bottom:5px;">&nbsp;</td>
-                    <td width="20%" align="right" style="padding-top:5px;padding-bottom:5px;">0.01</td>
+                    <td width="20%" align="right" style="padding-top:5px;padding-bottom:5px;">5.97</td>
                   </tr>
                 </table></td>
               </tr>
               <tr style="border-top:1px solid #999;border-bottom:1px solid #999;">
                 <td><table width="100%" border="0" cellspacing="0" cellpadding="0" style="margin:5px 0px;"><tr>
-                            <td width="70%" align="right"><strong>Cash</strong></td>
+                            <td width="70%" align="right"><strong>Credit Card</strong></td>
                             <td width="10%" align="right">&nbsp;</td>
-                            <td width="20%" align="right">0.01</td>
+                            <td width="20%" align="right">5.97</td>
                           </tr></table></td>
               </tr>
               <tr>
@@ -150,7 +150,7 @@
                   <tr style="border-top:1px solid #999;border-bottom:1px solid #999;">
                     <td width="70%" align="right" style="padding-top:5px;padding-bottom:5px;"><strong>Tendered</strong></td>
                     <td width="10%" align="right" style="padding-top:5px;padding-bottom:5px;">&nbsp;</td>
-                    <td width="20%" align="right" style="padding-top:5px;padding-bottom:5px;">0.01</td>
+                    <td width="20%" align="right" style="padding-top:5px;padding-bottom:5px;">5.97</td>
                   </tr>
                   <tr style="border-bottom:1px solid #999;">
                     <td width="70%" align="right" style="padding-top:5px;padding-bottom:5px;"><strong>Your Change</strong></td>
@@ -164,20 +164,20 @@
                   <tr>
                     <td width="49%" align="left"><strong>Cashier ID : </strong>101</td>
                     <td width="8%">&nbsp;</td>
-                    <td width="43%" align="right"><strong>Register : </strong>102</td>
+                    <td width="43%" align="right"><strong>Register : </strong>101</td>
                   </tr>
                   <tr>
-                    <td align="left"><strong>Tender Type : </strong>Cash</td>
+                    <td align="left"><strong>Tender Type : </strong>Credit Card</td>
                     <td>&nbsp;</td>
                     <td align="left">&nbsp;</td>
                   </tr>
                   <tr>
-                    <td align="left"><strong>TRN : </strong>2106101021</td>
+                    <td align="left"><strong>TRN : </strong>21053010185</td>
                     <td>&nbsp;</td>
                     <td align="left">&nbsp;</td>
                   </tr>
                   <tr>
-                    <td align="left"><strong>TRN Time : </strong>06-10-2021 11:29 AM</td>
+                    <td align="left"><strong>TRN Time : </strong>05-30-2021 05:57 PM</td>
                     <td>&nbsp;</td>
                     <td align="left">&nbsp;</td>
                   </tr>
