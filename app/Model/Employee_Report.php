9d08a31ddac5abe2d811751b2f8701aa2e131b91
Hanmanth <hanmanth.b@aroha.co.in-at-613112965612>
2021-09-08 19:09:21 +0530

    EMP issue

--- app/Model/Employee_Report.php
+++ app/Model/Employee_Report.php
@@ -4,25 +4,35 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use DateTime;
 
 class Employee_Report extends Model
 {
     protected $connection = 'mysql_dynamic';
 
     public function getCategoriesReport($sdate,$edate) {
-        $data['start_date'] = $sdate;
+        // $data['start_date'] = $sdate;
     
-        $data['end_date'] = $edate;
+        // $data['end_date'] = $edate;
+        
+        
+                $start_date = DateTime::createFromFormat('m-d-Y', $sdate);
+                $data['start_date'] = $start_date->format('Y-m-d');
+    
+                $end_date = DateTime::createFromFormat('m-d-Y', $edate);
+                $data['end_date'] = $end_date->format('Y-m-d');
+        
+        
     
         $sql="SELECT trn_s.vusername as vusername, (case when trn_s.vtendertype LIKE '%Return%' then 'Return' else trn_s.vtrntype end)as TrnType, trn_sd.isalesid as isalesid, date_format(trn_s.dtrandate,'%m-%d-%Y %H:%i:%s') as trn_date_time, trn_sd.vitemname as vitemname, trn_sd.nextunitprice as nextunitprice, trn_s.iuserid as iuserid, trn_sd.idettrnid as idettrnid, trn_sd.vitemcode as vitemcode, trn_sd.ndebitqty as ndebitqty, trn_sd.nunitprice as nunitprice, 'First Query' as QR 
             FROM trn_sales trn_s left join trn_salesdetail trn_sd on trn_s.isalesid=trn_sd.isalesid 
             where trn_s.ibatchid in                 
                             (select d.batchid from trn_endofday e join trn_endofdaydetail d on e.id=d.eodid 
-            where date_format(e.dstartdatetime,'%m-%d-%Y') between '".$data['start_date']."'  AND '".$data['end_date']."')
+            where date_format(e.dstartdatetime,'%Y-%m-%d') between '".$data['start_date']."'  AND '".$data['end_date']."')
             
             AND ((trn_s.vtrntype in ('Void','No Sale')) OR (trn_s.vtendertype LIKE '%Return%'))
             
-            AND date_format(trn_s.dtrandate,'%m-%d-%Y') between '".$data['start_date']."'  AND '".$data['end_date']."'
+            AND date_format(trn_s.dtrandate,'%Y-%m-%d') between '".$data['start_date']."'  AND '".$data['end_date']."'
             
             union all 
             
@@ -32,13 +42,15 @@ class Employee_Report extends Model
             from mst_deleteditem mdi LEFT JOIN mst_user mu ON(mu.iuserid=mdi.userid) 
             where 
             batchid in (select d.batchid from trn_endofday e join trn_endofdaydetail d on e.id=d.eodid 
-            where date_format(e.dstartdatetime,'%m-%d-%Y') between '".$data['start_date']."'  AND '".$data['end_date']."')
+            where date_format(e.dstartdatetime,'%Y-%m-%d') between '".$data['start_date']."'  AND '".$data['end_date']."')
             order by trn_date_time";
-            // date_format(mdi.LastUpdate,'%m-%d-%Y') >= '".$data['start_date']."'  AND date_format(mdi.LastUpdate,'%m-%d-%Y') <=  '".$data['end_date']."'
+            
+            
+            // date_format(mdi.LastUpdate,'%Y-%m-%d') >= '".$data['start_date']."'  AND date_format(mdi.LastUpdate,'%m-%d-%Y') <=  '".$data['end_date']."'
             // order by trn_date_time";
             
             
-            // dd($sql);
+           // dd($sql);
     
         $return_data = DB::connection('mysql_dynamic')->select($sql);
         $return = json_decode(json_encode($return_data), true); 	
